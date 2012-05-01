36d6c93d8ca2d2f919aff355c975b87a2a0505c1
Bai Xuesong <cedarbirdcn@hotmail.com>
2012-05-02 05:56:33 +0900

    add celsius.c

--- Project/chapter_02/src/celsius.c
+++ Project/chapter_02/src/celsius.c
@@ -7,12 +7,24 @@
  * provided that this copyright notice is retained.      *
  *********************************************************/
 
-/* pun.c (Chapter 2, page 10) */
+/* celsius.c (Chapter 2, page 24) */
+/* Converts a Fahrenheit temperature to Celsius */
 
 #include <stdio.h>
 
+#define FREEZING_PT 32.0f
+#define SCALE_FACTOR (5.0f / 9.0f)
+
 int main(void)
 {
-  printf("To C, or not to C: that is the question.\n");
+  float fahrenheit, celsius;
+
+  printf("Enter Fahrenheit temperature: ");
+  scanf("%f", &fahrenheit);
+
+  celsius = (fahrenheit - FREEZING_PT) * SCALE_FACTOR;
+
+  printf("Celsius equivalent: %.1f\n", celsius);
+
   return 0;
 }
