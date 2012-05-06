a2f4db56913c05df3c7f8cd3fe1048131f9c61f4
Bai Xuesong <cedarbirdcn@hotmail.com>
2012-05-06 20:20:37 +0900

    add dweight3

---
+++ Project/chapter_02/src/dweight3.c
@@ -0,0 +1,26 @@
+/*********************************************************
+ * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
+ * By K. N. King                                         *
+ * Copyright (c) 2008, 1996 W. W. Norton & Company, Inc. *
+ * All rights reserved.                                  *
+ * This program may be freely distributed for class use, *
+ * provided that this copyright notice is retained.      *
+ *********************************************************/
+
+/* dweight.c (Chapter 2, page 33) */
+/* Computes the dimensional weight of a 12" x 10" x 8" box */
+
+#include <stdio.h>
+
+int main(void)
+{
+  int height = 8, length = 12, width = 10, volume;
+
+  volume = height * length * width;
+
+  printf("Dimensions: %dx%dx%d\n", length, width, height);
+  printf("Volume (cubic inches): %d\n", volume);
+  printf("Dimensional weight (pounds): %d\n", (volume + 165)/166);
+
+  return 0;
+}
