c73d8a7504e9c02e22005f476cf4d9aa6c8a7bf1
Bai Xuesong <cedarbirdcn@hotmail.com>
2012-05-10 02:33:16 +0900

    add printnum.c

---
+++ Project/chapter_03/src/printnum.c
@@ -0,0 +1,23 @@
+/*********************************************************
+ * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
+ * By K. N. King                                         *
+ * Copyright (c) 2008, 1996 W. W. Norton & Company, Inc. *
+ * All rights reserved.                                  *
+ * This program may be freely distributed for class use, *
+ * provided that this copyright notice is retained.      *
+ *********************************************************/
+
+/* tprintf.c (Chapter 3, page 49) */
+/* Prints int and float values in various formats */
+
+#include <stdio.h>
+
+int main(void)
+{
+  printf("%6d, %4d\n", 86, 1040);
+  printf("%12.5e\n", 30.253);
+  printf("%.4f\n", 83.162);
+  printf("%-6.2g\n", .0000009979);
+
+  return 0;
+}
