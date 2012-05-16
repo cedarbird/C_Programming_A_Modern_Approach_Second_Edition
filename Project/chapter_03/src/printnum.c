e6232555769be3a02fb470ed577a409e7abbb93e
Bai Xuesong <cedarbirdcn@hotmail.com>
2012-05-16 22:15:38 +0900

    add some program in chapter 3

--- Project/chapter_03/src/printnum.c
+++ Project/chapter_03/src/printnum.c
@@ -1,13 +1,13 @@
 /*********************************************************
  * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
- * By K. N. King                                         *
+ * By XueSong.Bai                                        *
  * Copyright (c) 2008, 1996 W. W. Norton & Company, Inc. *
  * All rights reserved.                                  *
  * This program may be freely distributed for class use, *
  * provided that this copyright notice is retained.      *
  *********************************************************/
 
-/* tprintf.c (Chapter 3, page 49) */
+/* printnum.c (Chapter 3, page 49) */
 /* Prints int and float values in various formats */
 
 #include <stdio.h>
