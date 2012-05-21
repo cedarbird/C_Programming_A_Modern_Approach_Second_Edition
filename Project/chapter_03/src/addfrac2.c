77c58d0acbc5aac8c20d06f1902b421ab59e1af4
Bai Xuesong <cedarbirdcn@hotmail.com>
2012-05-22 00:49:27 +0900

    add some src

---
+++ Project/chapter_03/src/addfrac2.c
@@ -0,0 +1,27 @@
+/*********************************************************
+ * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
+ * By K. N. King                                         *
+ * Copyright (c) 2008, 1996 W. W. Norton & Company, Inc. *
+ * All rights reserved.                                  *
+ * This program may be freely distributed for class use, *
+ * provided that this copyright notice is retained.      *
+ *********************************************************/
+
+/* addfrac.c (Chapter 3, page 46) */
+/* Adds two fractions */
+
+#include <stdio.h>
+
+int main(void)
+{
+  int num1, denom1, num2, denom2, result_num, result_denom;
+
+  printf("Enter two fractions separated by a plus sign: ");
+  scanf("%d/%d+%d/%d", &num1, &denom1, &num2, &denom2);
+
+  result_num = num1 * denom2 + num2 * denom1;
+  result_denom = denom1 * denom2;
+  printf("The sum is %d/%d\n", result_num, result_denom);
+
+  return 0;
+}
---
+++ Project/chapter_03/src/magicsquare.c
@@ -0,0 +1,35 @@
+/*********************************************************
+ * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
+ * By XueSong.Bai                                        *
+ * Copyright (c) 2008, 1996 W. W. Norton & Company, Inc. *
+ * All rights reserved.                                  *
+ * This program may be freely distributed for class use, *
+ * provided that this copyright notice is retained.      *
+ *********************************************************/
+
+/* magicsquare.c (Chapter 3, page 50) */
+
+#include <stdio.h>
+
+int main(void)
+{
+  int digit_01, digit_02, digit_03, digit_04;
+  int digit_05, digit_06, digit_07, digit_08;
+  int digit_09, digit_10, digit_11, digit_12;
+  int digit_13, digit_14, digit_15, digit_16;
+
+  printf("Enter the number from 1 to 16 in any order:\n");
+  scanf("%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d", 
+         &digit_01, &digit_02, &digit_03, &digit_04,
+         &digit_05, &digit_06, &digit_07, &digit_08,
+         &digit_09, &digit_10, &digit_11, &digit_12,
+         &digit_13, &digit_14, &digit_15, &digit_16
+        );
+  
+  printf("%2d %2d %2d %2d\n", digit_01, digit_02, digit_03, digit_04);
+  printf("%2d %2d %2d %2d\n", digit_05, digit_06, digit_07, digit_08);
+  printf("%2d %2d %2d %2d\n", digit_09, digit_10, digit_11, digit_12);
+  printf("%2d %2d %2d %2d\n", digit_13, digit_14, digit_15, digit_16);
+
+  return 0;
+}
