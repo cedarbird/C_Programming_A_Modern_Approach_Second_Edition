cf96a13bc9d700eee6893f28d946d3a1afb0c8e9
Bai Xuesong <cedarbirdcn@hotmail.com>
2012-05-22 23:30:04 +0900

    add section41

---
+++ Project/chapter_04/src/section41.c
@@ -0,0 +1,40 @@
+/*********************************************************
+ * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
+ * By XueSong.Bai                                        *
+ * Copyright (c) 2008, 1996 W. W. Norton & Company, Inc. *
+ * All rights reserved.                                  *
+ * This program may be freely distributed for class use, *
+ * provided that this copyright notice is retained.      *
+ *********************************************************/
+
+/* section41.c (Chapter 4, page 68) */
+
+#include <stdio.h>
+
+int main(void)
+{
+  int i, j, k;
+
+  i = 5;
+  j = 3;
+
+  printf("%d %d\n", i / j, i % j);
+
+  i = 2;
+  j = 3;
+
+  printf("%d\n", (i + 10) % j);
+
+  i = 7;
+  j = 8;
+  k = 9;
+
+  printf("%d\n", (i + 10) % k / j);
+
+  i = 1;
+  j = 2;
+  k = 3;
+
+  printf("%d\n", (i + 5) % (j + 2) / k);
+  return 0;
+}
