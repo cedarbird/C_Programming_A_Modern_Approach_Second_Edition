5e4184259d1f11094964329c31a4e84b89a8c728
Bai Xuesong <cedarbirdcn@hotmail.com>
2012-05-29 00:25:45 +0900

    add upc2.c

---
+++ Project/chapter_04/src/upc2.c
@@ -0,0 +1,26 @@
+/*********************************************************
+ * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
+ * By XueSong.Bai                                        *
+ * Copyright (c) 2008, 1996 W. W. Norton & Company, Inc. *
+ * All rights reserved.                                  *
+ * This program may be freely distributed for class use, *
+ * provided that this copyright notice is retained.      *
+ *********************************************************/
+
+/* upc2.c (Chapter 4, page 71) */
+
+#include <stdio.h>
+
+int main(void)
+{
+  int d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11;
+  int check_digit;
+
+  printf("Enter the first 11 digits of a UPC: ");
+  scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d", &d1, &d2, &d3, &d4, &d5, &d6, &d7, &d8, &d9, &d10, &d11);
+
+  check_digit = 9 - ((3 * (d1 + d3 + d5 + d7 + d9 + d11) + (d2 + d4 + d6 +d8 + d10) - 1) % 10); 
+
+  printf("Check digit: %d\n", check_digit);
+  return 0;
+}
