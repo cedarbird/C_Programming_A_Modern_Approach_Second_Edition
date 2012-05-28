862a991b9a3b334ead9d1c117b4fabf1a0775dc0
Bai Xuesong <cedarbirdcn@hotmail.com>
2012-05-29 00:04:50 +0900

    add program

---
+++ Project/chapter_04/src/convert_to_octal.c
@@ -0,0 +1,36 @@
+/*********************************************************
+ * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
+ * By XueSong.Bai                                        *
+ * Copyright (c) 2008, 1996 W. W. Norton & Company, Inc. *
+ * All rights reserved.                                  *
+ * This program may be freely distributed for class use, *
+ * provided that this copyright notice is retained.      *
+ *********************************************************/
+
+/* convert_to_octal.c (Chapter 4, page 71) */
+
+#define OCTAL 8
+
+#include <stdio.h>
+
+int main(void)
+{
+  int i;
+  int d1, d2, d3, d4, d5;
+
+  printf("Enter a number between 0 and 32767: ");
+  scanf("%d", &i);
+
+  d5 = i % OCTAL;
+  i /= OCTAL;
+  d4 = i % OCTAL;
+  i /= OCTAL;
+  d3 = i % OCTAL;
+  i /= OCTAL;
+  d2 = i % OCTAL;
+  i /= OCTAL;
+  d1 = i % OCTAL;
+  
+  printf("In octal, your number is: %d%d%d%d%d\n", d1, d2, d3, d4, d5);
+  return 0;
+}
---
+++ Project/chapter_04/src/reversaldigit3_2.c
@@ -0,0 +1,24 @@
+/*********************************************************
+ * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
+ * By XueSong.Bai                                        *
+ * Copyright (c) 2008, 1996 W. W. Norton & Company, Inc. *
+ * All rights reserved.                                  *
+ * This program may be freely distributed for class use, *
+ * provided that this copyright notice is retained.      *
+ *********************************************************/
+
+/* reversaldigit3_2.c (Chapter 4, page 71) */
+
+#include <stdio.h>
+
+int main(void)
+{
+  int n1, n2, n3;
+
+  printf("Enter a three-digit number: ");
+  scanf("%1d%1d%1d", &n1, &n2, &n3);
+  printf("The reversal is: %d%d%d\n", n3, n2, n1);
+
+  return 0;
+}
+
