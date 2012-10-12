afe1a74531b4131eae1ecbf8d04069b25633021e
root <cedarbirdcn@hotmail.com>
2012-10-12 14:47:29 +0900

    add chapter 05

---
+++ Project/chapter_05/src/lettergrade.c
@@ -0,0 +1,40 @@
+/*********************************************************
+ * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
+ * By K. N. King                                         *
+ * Copyright (c) 2008, 1996 W. W. Norton & Company, Inc. *
+ * All rights reserved.                                  *
+ * This program may be freely distributed for class use, *
+ * provided that this copyright notice is retained.      *
+ *********************************************************/
+
+/* lettergrade.c (Chapter 5, page 97) */
+
+#include <stdio.h>
+
+int main(void)
+{
+  int digit, tendigit;
+  char lettergrade;
+  printf("enter numerical grade.\n");
+  scanf("%d", &digit);
+
+  if ( 0 > digit || 100 <= digit) {
+    printf("error!\nyour must input a postive number in 0-99.\n");
+    return -1;
+  }
+
+  tendigit = digit/10;
+
+  switch (tendigit) {
+    case 9:  lettergrade = 'A'; break;
+    case 8:  lettergrade = 'B'; break;
+    case 7:  lettergrade = 'C'; break;
+    case 6:  lettergrade = 'D'; break;
+    default: lettergrade = 'F';
+  }
+
+  printf("Letter Grade: %c\n", lettergrade);
+
+  return 0;
+}
+
