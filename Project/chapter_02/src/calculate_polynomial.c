1818f97516accf1701a5612e4feccad8f8bb1e8e
cedarbird <cedarbird@cedarbird-ubuntu.(none)>
2012-11-24 18:43:29 +0900

    modify format

--- Project/chapter_02/src/calculate_polynomial.c
+++ Project/chapter_02/src/calculate_polynomial.c
@@ -16,7 +16,8 @@ int main(void)
   int x;
   printf("Enter a x values:");
   scanf("%d", &x);
-  printf("The polynomial 3x^5+2x^4-5x^3-x^2+7x-6: %d\n", (3 * x * x * x * x * x + 2 * x * x * x * x - 5 * x * x * x - x * x + 7 * x -6));
+  printf("The polynomial 3x^5+2x^4-5x^3-x^2+7x-6: %d\n",
+    (3 * x * x * x * x * x + 2 * x * x * x * x - 5 * x * x * x - x * x + 7 * x -6));
 
   return 0;
 }
--- Project/chapter_02/src/calculate_polynomial2.c
+++ Project/chapter_02/src/calculate_polynomial2.c
@@ -17,7 +17,8 @@ int main(void)
   int x;
   printf("Enter a x values:");
   scanf("%d", &x);
-  printf("The polynomial 3x^5+2x^4-5x^3-x^2+7x-6: %d\n", (((((3 * x + 2) * x - 5) * x  - 1)  * x + 7) * x -6));
+  printf("The polynomial 3x^5+2x^4-5x^3-x^2+7x-6: %d\n",
+    (((((3 * x + 2) * x - 5) * x  - 1)  * x + 7) * x -6));
 
   return 0;
 }
