ea30cfa2c4bf303e6c20a10c986edc7b3b8d326a
root <cedarbirdcn@hotmail.com>
2012-11-28 17:46:30 +0900

    add chapter 05 project

--- Project/chapter_05/src/flight.c
+++ Project/chapter_05/src/flight.c
@@ -20,7 +20,9 @@ int main(void)
 
     minutes_from_midnight = hours * 60 + minutes;
 
-    if (minutes_from_midnight < 616) {
+    if (minutes_from_midnight < 480)
+      printf("out of service time.\n");
+    else if (minutes_from_midnight < 616) {
       printf("Closest departure time is 8:00 AM, arriving at 10:16 AM.\n");
     } else if (minutes_from_midnight < 712) {
       printf("Closest departure time is 9:43 AM, arriving at 11:52 AM.\n");
@@ -37,7 +39,7 @@ int main(void)
     } else if (minutes_from_midnight < 1438) {
       printf("Closest departure time is 9:45 PM, arriving at 11:58 PM.\n");
     } else {
-      printf("the service of today was over.\n");
+      printf("out of service time.\n");
     }
       
     return 0;
