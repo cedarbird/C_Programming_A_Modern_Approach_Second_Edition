8f88d2324271d570f5a355c614681a27de3d17cf
root <cedarbirdcn@hotmail.com>
2012-12-14 19:55:40 +0900

    chapter 06

---
+++ Project/chapter_06/src/show_max.c
@@ -0,0 +1,33 @@
+/*********************************************************
+ * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
+ * By K. N. King                                         *
+ * Copyright (c) 2008, 1996 W. W. Norton & Company, Inc. *
+ * All rights reserved.                                  *
+ * This program may be freely distributed for class use, *
+ * provided that this copyright notice is retained.      *
+ *********************************************************/
+
+/* show_max.c (Chapter 6, page 122) */
+/* infinite loop */
+
+#include <stdio.h>
+
+int main(void)
+  {
+    float input, max = 0.0f;
+
+    do {
+      printf("Enter a number: ");
+      scanf("%f", &input);
+      if (input <= 0.0f)
+        break;
+      else
+        if (input > max)
+          max = input;
+    } while(1);
+
+    printf("The largest number entered was %f.\n", max); 
+      
+    return 0;
+  }
+
