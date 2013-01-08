066090ad30f7504b282b23e0080d8988ab885514
root <cedarbirdcn@hotmail.com>
2013-01-08 15:33:05 +0900

    add chapter 07

---
+++ Project/chapter_07/src/section75.c
@@ -0,0 +1,23 @@
+/*********************************************************
+ * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
+ * By K. N. King                                         *
+ * Copyright (c) 2008, 1996 W. W. Norton & Company, Inc. *
+ * All rights reserved.                                  *
+ * This program may be freely distributed for class use, *
+ * provided that this copyright notice is retained.      *
+ *********************************************************/
+
+/* section71.c (Chapter 7, page 155) */
+
+#include <stdio.h>
+
+int main(void)
+  {
+    printf("%d.\n", 'A');
+//  printf("%d.\n", 0b1000001'); /* wrong bit number */
+    printf("%d.\n", 0101);
+    printf("%d.\n", 0x41);
+
+    return 0;
+  }
+
