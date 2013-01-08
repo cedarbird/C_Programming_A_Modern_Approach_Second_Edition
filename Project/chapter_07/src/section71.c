8f1657ec59ee3de546e1cb05b68516903dd79138
root <cedarbirdcn@hotmail.com>
2013-01-08 14:35:49 +0900

    add chapter 07

---
+++ Project/chapter_07/src/section71.c
@@ -0,0 +1,21 @@
+/*********************************************************
+ * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
+ * By K. N. King                                         *
+ * Copyright (c) 2008, 1996 W. W. Norton & Company, Inc. *
+ * All rights reserved.                                  *
+ * This program may be freely distributed for class use, *
+ * provided that this copyright notice is retained.      *
+ *********************************************************/
+
+/* section71.c (Chapter 7, page 155) */
+
+#include <stdio.h>
+
+int main(void)
+  {
+    int a = 077, b = 0x77, c = 0XABC;
+    printf("%d %d %d.\n", a, b, c);
+
+    return 0;
+  }
+
