d1a5870a070b125d1887047b889fb686f28b4689
root <cedarbirdcn@hotmail.com>
2013-01-09 16:11:09 +0900

    add chapter 07

---
+++ Project/chapter_07/src/cal_avg_length.c
@@ -0,0 +1,33 @@
+/*********************************************************
+ * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
+ * By K. N. King                                         *
+ * Copyright (c) 2008, 1996 W. W. Norton & Company, Inc. *
+ * All rights reserved.                                  *
+ * This program may be freely distributed for class use, *
+ * provided that this copyright notice is retained.      *
+ *********************************************************/
+
+/* cal_avg_length.c (Chapter 7, page 159) */
+
+#include <stdio.h>
+
+int main(void)
+  {
+    int words = 0, lengths = 0;
+    char prech = ' ', ch;
+
+    printf("Enter some word: ");
+    while((ch = getchar()) != '\n') {
+      if (ch != ' ')
+        lengths++;
+      else if (prech != ' ')
+        words++;
+      prech = ch;
+    }
+    words++;
+ 
+    printf("Avarage Length is: %.1f\n", (float) lengths / words);
+
+    return 0;
+  }
+
