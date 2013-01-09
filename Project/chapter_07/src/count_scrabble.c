c7ade3c0b259ef7cbc096dce2d7b94e037e0b74c
root <cedarbirdcn@hotmail.com>
2013-01-09 12:51:45 +0900

    add chapter 07

--- Project/chapter_07/src/count_scrabble.c
+++ Project/chapter_07/src/count_scrabble.c
@@ -19,28 +19,25 @@ int main(void)
 
     printf("Enter a word: ");
 
-    do {
-      ch = getchar();
-      if (ch >= 'a' && ch <= 'z')
-        ch = toupper(ch);
-      if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'L' || ch == 'N'
-       || ch == 'O' || ch == 'R' || ch == 'S' || ch == 'T' || ch == 'U')
-        value += 1;
-      else if (ch == 'D' || ch == 'G')
-        value += 2;
-      else if (ch == 'B' || ch == 'C' || ch == 'M' || ch == 'P')
-        value += 3;
-      else if (ch == 'F' || ch == 'H' || ch == 'V' || ch == 'W' || ch == 'Y')
-        value += 4;
-      else if (ch == 'K')
-        value += 5;
-      else if (ch == 'J' || ch == 'X')
-        value += 8;
-      else if (ch == 'Q' || ch == 'Z')
-        value += 10;
-    } while (ch != '\n');
+    while ((ch = getchar()) != '\n')
+      switch (toupper(ch)) {
+        case 'D': case 'G':
+          value += 2; break;
+        case 'B': case 'C': case 'M': case 'P':
+          value += 3; break;
+        case 'F': case 'H': case 'V': case 'W': case 'Y':
+          value += 4; break;
+        case 'K':
+          value += 5; break;
+        case 'J': case 'X':
+          value += 8; break;
+        case 'Q': case 'Z':
+          value += 10; break;
+        default:
+          value++; break;
+      }
 
-    printf("scrabble value: %d\n", value);
+    printf("Scrabble value: %d\n", value);
 
     return 0;
   }
