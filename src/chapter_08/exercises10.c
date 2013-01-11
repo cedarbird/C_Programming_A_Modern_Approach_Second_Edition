98a60332666b3a97afe0a2cf81de93df62619721
root <cedarbirdcn@hotmail.com>
2013-01-11 18:07:35 +0900

    add chapter 08

--- src/chapter_08/exercises10.c
+++ src/chapter_08/exercises10.c
@@ -7,31 +7,32 @@
  * provided that this copyright notice is retained.      *
  *********************************************************/
 
-/* exercises10.c (Chapter 8, page 178) */
+/* exercises10.c (Chapter 08, page 178) */
 /* show chess_board */
 
 #include <stdio.h>
 
-#define ROWS 8 
-#define COLS 8 
+#define ROWS 8
+#define COLS 8
 
 int main(void)
   {
-    char chess_board[ROWS][COLS] = { { 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r' },
-                                     { 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p' },
-                                     { ' ', '.', ' ', '.', ' ', '.', ' ', '.' },
-                                     { '.', ' ', '.', ' ', '.', ' ', '.', ' ' },
-                                     { ' ', '.', ' ', '.', ' ', '.', ' ', '.' },
-                                     { '.', ' ', '.', ' ', '.', ' ', '.', ' ' },
-                                     { 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P' },
-                                     { 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R' } };
-  
+    char chess_board[ROWS][COLS] = {{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+                                    {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+                                    {' ', '.', ' ', '.', ' ', '.', ' ', '.'},
+                                    {'.', ' ', '.', ' ', '.', ' ', '.', ' '},
+                                    {' ', '.', ' ', '.', ' ', '.', ' ', '.'},
+                                    {'.', ' ', '.', ' ', '.', ' ', '.', ' '},
+                                    {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+                                    {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}};
+
+    printf("Chess Board:\n");
     for (int i = 0; i < ROWS; i++) {
       for (int j = 0; j < COLS; j++)
         printf("%2c", chess_board[i][j]);
       printf("\n");
     }
-  
+
     return 0;
   }
 
