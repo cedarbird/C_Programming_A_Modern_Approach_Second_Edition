5366e64df99576bf2b7bef0b6aa6ae75593fa5c2
root <cedarbirdcn@hotmail.com>
2013-01-11 18:10:11 +0900

    add chapter 08

--- src/chapter_08/exercises11.c
+++ src/chapter_08/exercises11.c
@@ -7,20 +7,21 @@
  * provided that this copyright notice is retained.      *
  *********************************************************/
 
-/* exercises11.c (Chapter 8, page 178) */
-/* show checker_board(cross A/B) */
+/* exercises11.c (Chapter 08, page 178) */
+/* show checker_board(A/B) */
 
 #include <stdio.h>
 
-#define ROWS 8 
-#define COLS 8 
+#define ROWS 8
+#define COLS 8
 #define ODD_CHAR 'A'
 #define EVEN_CHAR 'B'
 
 int main(void)
   {
     char checker_board[ROWS][COLS];
-  
+
+    /* fill checker board */
     for (int i = 0; i < ROWS; i++)
       for (int j = 0; j < COLS; j++) {
         if ((i + j) % 2 == 0)
@@ -29,12 +30,13 @@ int main(void)
           checker_board[i][j] = ODD_CHAR;
       }
 
+    printf("Check Board:\n");
     for (int i = 0; i < ROWS; i++) {
       for (int j = 0; j < COLS; j++)
         printf("%2c", checker_board[i][j]);
       printf("\n");
     }
-  
+
     return 0;
   }
 
