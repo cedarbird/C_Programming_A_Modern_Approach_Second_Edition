81e95c2897981823f6da901534f42cf143bc15c4
root <cedarbirdcn@hotmail.com>
2013-01-15 10:19:06 +0900

    add chapter 08

--- src/chapter_08/deal.c
+++ src/chapter_08/deal.c
@@ -9,6 +9,7 @@
 
 /* deal.c (Chapter 08, page 173) */
 /* Deals a random hand of cards */
+/* Warning: a missing braces around initializer in the 23 line */
 
 #include <stdbool.h>   /* C99 only */
 #include <stdio.h>
@@ -19,30 +20,30 @@
 #define NUM_RANKS 13
 
 int main(void)
-  {
-    bool in_hand[NUM_SUITS][NUM_RANKS] = {false};
-    int num_cards, rank, suit;
-    const char rank_code[] = {'2','3','4','5','6','7','8',
-                              '9','t','j','q','k','a'};
-    const char suit_code[] = {'c','d','h','s'};
+{
+  bool in_hand[NUM_SUITS][NUM_RANKS] = {false};
+  int num_cards, rank, suit;
+  const char rank_code[] = {'2','3','4','5','6','7','8',
+                            '9','t','j','q','k','a'};
+  const char suit_code[] = {'c','d','h','s'};
 
-    srand((unsigned) time(NULL));
+  srand((unsigned) time(NULL));
 
-    printf("Enter number of cards in hand: ");
-    scanf("%d", &num_cards);
+  printf("Enter number of cards in hand: ");
+  scanf("%d", &num_cards);
 
-    printf("Your hand:");
-    while (num_cards > 0) {
-      suit = rand() % NUM_SUITS;     /* picks a random suit */
-      rank = rand() % NUM_RANKS;     /* picks a random rank */
-      if (!in_hand[suit][rank]) {
-        in_hand[suit][rank] = true;
-        num_cards--;
-        printf(" %c%c", rank_code[rank], suit_code[suit]);
-      }
+  printf("Your hand:");
+  while (num_cards > 0) {
+    suit = rand() % NUM_SUITS;     /* picks a random suit */
+    rank = rand() % NUM_RANKS;     /* picks a random rank */
+    if (!in_hand[suit][rank]) {
+      in_hand[suit][rank] = true;
+      num_cards--;
+      printf(" %c%c", rank_code[rank], suit_code[suit]);
     }
-    printf("\n");
-
-    return 0;
   }
+  printf("\n");
+
+  return 0;
+}
 
--- src/chapter_08/repdigit.c
+++ src/chapter_08/repdigit.c
@@ -14,27 +14,27 @@
 #include <stdio.h>
 
 int main(void)
-  {
-    bool digit_seen[10] = {false};
-    int digit;
-    long n;
+{
+  bool digit_seen[10] = {false};
+  int digit;
+  long n;
 
-    printf("Enter a number: ");
-    scanf("%ld", &n);
+  printf("Enter a number: ");
+  scanf("%ld", &n);
 
-    while (n > 0) {
-      digit = n % 10;
-      if (digit_seen[digit])
-        break;
-      digit_seen[digit] = true;
-      n /= 10;
-    }
-
-    if (n > 0)
-      printf("Repeated digit\n");
-    else
-      printf("No repeated digit\n");
-
-    return 0;
+  while (n > 0) {
+    digit = n % 10;
+    if (digit_seen[digit])
+      break;
+    digit_seen[digit] = true;
+    n /= 10;
   }
+
+  if (n > 0)
+    printf("Repeated digit\n");
+  else
+    printf("No repeated digit\n");
+
+  return 0;
+}
 
