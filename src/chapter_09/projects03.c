b2662e052e0ed4f08dd7829b1a3b5d41cb0267cb
Bai Xuesong <cedarbirdcn@hotmail.com>
2013-01-25 01:40:47 +0900

    add chapter 09

--- src/chapter_09/projects03.c
+++ src/chapter_09/projects03.c
@@ -15,76 +15,60 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define SIDE 10
-#define UP 0
-#define DOWN 1
-#define LEFT 2
-#define RIGHT 3
-#define DIRECTION 4
-#define WALK_DISTANCE ('Z' - 'A' + 1)
+#define Y 10
+#define X 10
+#define NUMS_OF_DIRECTION 4
+#define DISTANCE ('Z' - 'A' + 1)
+#define POINT '.'
 
-void generate_random_walk(char walk[SIDE][SIDE]);
-void print_array(char walk[SIDE][SIDE]);
+void generate_random_walk(char walk[Y][X]);
+void print_array(char walk[Y][X]);
 
 int main(void)
   {
-    char walk[SIDE][SIDE];
+    char walk[Y][X];
+
     generate_random_walk(walk);
     print_array(walk);
 
     return 0;
   }
 
-void generate_random_walk(char walk[SIDE][SIDE])
+void generate_random_walk(char walk[Y][X])
   {
-    char ch = 'A'; /* start alphabet */
-    int x = 0, y = 0, nums_of_selection; /* initial direct and position */
-    bool position[DIRECTION] = {false};
+    char ch = 'A';
+    int i, j, x = 0, y = 0, nums_of_selection;
+    int axis[][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; /* UP/DOWN/LEFT/RIGHT{y, x} */
 
-    /* walk initializer */
-    for (int i = 0; i < SIDE; i++)
-      for (int j = 0; j < SIDE; j++)
-        walk[i][j] = '.';
+    for (i = 0; i < Y; i++)
+      for (j = 0; j < X; j++)
+        walk[i][j] = POINT;
     walk[y][x] = ch;
 
-    /* random initializer */
     srand((unsigned) time(NULL));
-
-    /* main logic */
-    for (int i = 0; i < WALK_DISTANCE - 1; i++) {
-
-      /* initialize */
+    for (i = 0; i < DISTANCE - 1; i++) {
       nums_of_selection = 0;
+      int next_axis[NUMS_OF_DIRECTION];
+      for (j = 0; j < NUMS_OF_DIRECTION; j++) {
+        if ((x + axis[j][1] >= 0 && x + axis[j][1] < X)
+         && (y + axis[j][0] >= 0 && y + axis[j][0] < Y)
+         && (walk[y + axis[j][0]][x + axis[j][1]] == POINT))
+          next_axis[nums_of_selection++] = j;
+      }
 
-      /* get info for effective selection */
-      if (y != 0 && walk[y - 1][x] == '.')
-        position[UP] = true;
-      if (y != (SIDE - 1) && walk[y + 1][x] == '.')
-        position[nums_of_selection++] = DOWN;
-      if (x != 0 && walk[y][x - 1] == '.')
-        position[nums_of_selection++] = LEFT;
-      if (x != (SIDE - 1) && walk[y][x + 1] == '.')
-        position[nums_of_selection++] = RIGHT;
-
-      /* stop when no way */
       if (nums_of_selection == 0)
         break;
 
-      /* make a random direction decision */
-      switch((position[rand() % nums_of_selection])) {
-        case UP:    walk[--y][x] = ++ch; break;
-        case DOWN:  walk[++y][x] = ++ch; break;
-        case LEFT:  walk[y][--x] = ++ch; break;
-        case RIGHT: walk[y][++x] = ++ch; break;
-      }
+      j = rand() % nums_of_selection;
+      walk[(y += axis[next_axis[j]][0])][(x += axis[next_axis[j]][1])] = ++ch;
     }
   }
 
-void print_array(char walk[SIDE][SIDE])
+void print_array(char walk[Y][X])
   {
-    /* show result */
-    for (int i = 0; i < SIDE; i++) {
-      for (int j = 0; j < SIDE; j++)
+    int i, j;
+    for (i = 0; i < Y; i++) {
+      for (j = 0; j < X; j++)
         printf("%2c", walk[i][j]);
       printf("\n");
     }
---
+++ src/chapter_09/projects08.c
@@ -0,0 +1,72 @@
+/*********************************************************
+ * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
+ * By K. N. King                                         *
+ * Copyright (c) 2008, 1996 W. W. Norton & Company, Inc. *
+ * All rights reserved.                                  *
+ * This program may be freely distributed for class use, *
+ * provided that this copyright notice is retained.      *
+ *********************************************************/
+
+/* projects04.c (Chapter 09, page 217) */
+/* random walk across 10 X 10 array */
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+
+#define NUMS_OF_DIRECTION 6
+
+int roll_dice(void);
+bool play_game(void);
+
+int main(void)
+  {
+    int wins = 0, losses = 0;
+
+    srand((unsigned) time(NULL));
+    for (;;) {
+      if (play_game()) wins++;
+      else losses++;
+      printf("\nPlay again? ");
+      if (toupper(getchar()) != 'Y') {
+        printf("Wins: %d  Losses: %d", wins, losses);
+        break;
+      }
+      while (getchar() != '\n') /* skip the left input end to newline */
+        ;
+      printf("\n");
+    }
+
+    return 0;
+  }
+
+bool play_game(void)
+  {
+    int i = 0, point, dice;
+    for (;;) {
+      dice = roll_dice();
+      if (i == 0) {
+        switch (dice) {
+          case 7: case 11: printf("You win!\n"); return true;
+          case 2: case 3: case 12: printf("You lose!\n"); return false;
+          default: printf("Your point is %d\n", (point = dice)); i++; break;
+        }
+      } else if (dice == point) {
+        printf("You win!\n");
+        return true;
+      } else if (dice == 7) {
+        printf("You lose!\n");
+        return false;
+      }
+    }
+  }
+
+int roll_dice(void)
+  {
+    int dice = rand() % NUMS_OF_DIRECTION + 1;
+    printf("You rolled: %d\n", (dice += rand() % NUMS_OF_DIRECTION + 1));
+    return dice;
+  }
+
