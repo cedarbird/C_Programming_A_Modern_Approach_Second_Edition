0f5aa649a56884c6457b98b74e0756cdb7cc8273
root <cedarbirdcn@hotmail.com>
2013-02-06 16:14:30 +0900

    modify chapter 02

---
+++ src/chapter_02/exercises04.c
@@ -0,0 +1,31 @@
+/*********************************************************
+ * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
+ * By K. N. King                                         *
+ * Copyright (c) 2008, 1996 W. W. Norton & Company, Inc. *
+ * All rights reserved.                                  *
+ * This program may be freely distributed for class use, *
+ * provided that this copyright notice is retained.      *
+ *********************************************************/
+
+/* exercises01.c (Chapter 2, page 33) */
+/* Here's one possible program: */
+/* Warning: it's wrong to use uninitialized variable */
+
+#include <stdio.h>
+
+int main(void)
+{
+  int i, j, k;
+  float x, y, z;
+
+  printf("Value of i: %d\n", i);
+  printf("Value of j: %d\n", j);
+  printf("Value of k: %d\n", k);
+
+  printf("Value of x: %g\n", x);
+  printf("Value of y: %g\n", y);
+  printf("Value of z: %g\n", z);
+
+  return 0;
+}
+
