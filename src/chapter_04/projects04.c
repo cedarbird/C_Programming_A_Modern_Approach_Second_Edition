d048d9b925af8688417c189d83d4c27b6cc6f0ba
cedarbird <cedarbirdcn@hotmail.com>
2013-02-19 21:18:06 +0900

    modify chapter 04

--- src/chapter_04/projects04.c
+++ src/chapter_04/projects04.c
@@ -7,31 +7,32 @@
  * provided that this copyright notice is retained.      *
  *********************************************************/
 
-/* convert_to_octal.c (Chapter 04, page 71) */
+/* projects04.c (Chapter 04, page 71) */
+/* Converts Decimal number to Octal number */
 
 #define OCTAL 8
 
 #include <stdio.h>
 
 int main(void)
-{
-  int i;
-  int d1, d2, d3, d4, d5;
+  {
+    int i;
+    int d1, d2, d3, d4, d5;
 
-  printf("Enter a number between 0 and 32767: ");
-  scanf("%d", &i);
+    printf("Enter a number between 0 and 32767: ");
+    scanf("%d", &i);
 
-  d5 = i % OCTAL;
-  i /= OCTAL;
-  d4 = i % OCTAL;
-  i /= OCTAL;
-  d3 = i % OCTAL;
-  i /= OCTAL;
-  d2 = i % OCTAL;
-  i /= OCTAL;
-  d1 = i % OCTAL;
-  
-  printf("In octal, your number is: %d%d%d%d%d\n", d1, d2, d3, d4, d5);
-  return 0;
-}
+    d5 = i % OCTAL;
+    i /= OCTAL;
+    d4 = i % OCTAL;
+    i /= OCTAL;
+    d3 = i % OCTAL;
+    i /= OCTAL;
+    d2 = i % OCTAL;
+    i /= OCTAL;
+    d1 = i % OCTAL;
+
+    printf("In octal, your number is: %d%d%d%d%d\n", d1, d2, d3, d4, d5);
+    return 0;
+  }
 
--- src/chapter_04/projects05.c
+++ src/chapter_04/projects05.c
@@ -7,7 +7,7 @@
  * provided that this copyright notice is retained.      *
  *********************************************************/
 
-/* upc2.c (Chapter 04, page 71) */
+/* projects05.c (Chapter 04, page 71) */
 
 #include <stdio.h>
 
@@ -17,11 +17,14 @@ int main(void)
   int check_digit;
 
   printf("Enter the first 11 digits of a UPC: ");
-  scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d", &d1, &d2, &d3, &d4, &d5, &d6, &d7, &d8, &d9, &d10, &d11);
+  scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d",
+         &d1, &d2, &d3, &d4, &d5, &d6, &d7, &d8, &d9, &d10, &d11);
 
-  check_digit = 9 - ((3 * (d1 + d3 + d5 + d7 + d9 + d11) + (d2 + d4 + d6 +d8 + d10) - 1) % 10); 
+  check_digit = 9 - ((3 * (d1 + d3 + d5 + d7 + d9 + d11)
+                        + (d2 + d4 + d6 + d8 + d10) - 1) % 10);
 
   printf("Check digit: %d\n", check_digit);
+
   return 0;
 }
 
