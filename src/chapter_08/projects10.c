748240fe85ed5c68e005ddb5fc0d512441606baf
root <cedarbirdcn@hotmail.com>
2013-03-15 17:56:04 +0900

    modify chapter 08

--- src/chapter_08/projects10.c
+++ src/chapter_08/projects10.c
@@ -7,37 +7,27 @@
  * provided that this copyright notice is retained.      *
  *********************************************************/
 
-/* projects08.c (Chapter 05, page 96) */
+/* projects10.c (Chapter 08, page 180) */
 /* Displays the departure and arrival time for the flght whose departue time
    is closest to that entered bu the user */
 
 #include <stdio.h>
+#define NUM_FLIGHTS ((int) sizeof(departure_times) / sizeof(departure_times[0]))
 
 int main(void)
 {
   int hours, minutes, minutes_from_midnight;
+  const int departure_times[] = {480, 583, 679, 767, 840, 945,  1140, 1305},
+            arrival_times[]   = {616, 712, 811, 900, 968, 1075, 1280, 1438};
 
   printf("Enter a 24-hour time: ");
   scanf("%d :%d", &hours, &minutes);
 
   minutes_from_midnight = hours * 60 + minutes;
 
-  if (minutes_from_midnight < 8 * 60 || minutes_from_midnight >= 21 * 60 + 45)
-    printf("Closest departure time is 9:45 PM, arriving at 11:58 PM.\n");
-  else if (minutes_from_midnight < 9 * 60 + 43)
-    printf("Closest departure time is 8:00 AM, arriving at 10:16 AM.\n");
-  else if (minutes_from_midnight < 11 * 60 + 19)
-    printf("Closest departure time is 9:43 AM, arriving at 11:52 AM.\n");
-  else if (minutes_from_midnight < 12 * 60 + 47)
-    printf("Closest departure time is 11:19 AM, arriving at 1:31 PM.\n");
-  else if (minutes_from_midnight < 14 * 60)
-    printf("Closest departure time is 12:47 PM, arriving at 3:00 PM.\n");
-  else if (minutes_from_midnight < 15 * 60 + 45)
-    printf("Closest departure time is 2:00 PM, arriving at 4:08 PM.\n");
-  else if (minutes_from_midnight < 19 * 60)
-    printf("Closest departure time is 3:45 PM, arriving at 5:55 PM.\n");
-  else
-    printf("Closest departure time is 7:00 PM, arriving at 9:20 PM.\n");
+  for (int i = 0; i < NUM_FLIGHTS; i++)
+    if (minutes_from_midnight > departure_times[i])
+      
 
   return 0;
 }
