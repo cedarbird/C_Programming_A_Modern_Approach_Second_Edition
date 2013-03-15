1dc3eaae8d45fff4be7586f3ce7815595d51601d
root <cedarbirdcn@hotmail.com>
2013-03-15 10:01:07 +0900

    modify chapter 08

--- src/chapter_08/projects02.c
+++ src/chapter_08/projects02.c
@@ -8,35 +8,33 @@
  *********************************************************/
 
 /* projects02.c (Chapter 08, page 178) */
-/* count repeated digits */
+/* Shows how many times each digit appears in the number. */
 
 #include <stdio.h>
 
-#define NUMS_OF_DECIMAL 10
-
 int main(void)
-  {
-    int digit_count[NUMS_OF_DECIMAL] = {0};
-    int digit;
-    long n;
+{
+  int digit_count[10] = {0};
+  int digit;
+  long n;
 
-    printf("Enter a number: ");
-    scanf("%ld", &n);
+  printf("Enter a number: ");
+  scanf("%ld", &n);
 
-    while (n > 0) {
-      digit = n % NUMS_OF_DECIMAL;
-      digit_count[digit]++;
-      n /= NUMS_OF_DECIMAL;
-    }
-
-    printf("Digit:      ");
-    for (digit  = 0; digit < NUMS_OF_DECIMAL; digit++)
-      printf("%3d", digit);
-    printf("\nOccurences: ");
-    for (digit = 0; digit < NUMS_OF_DECIMAL; digit++)
-      printf("%3d", digit_count[digit]);
-    printf("\n");
-
-    return 0;
+  while (n > 0) {
+    digit = n % 10;
+    digit_count[digit]++;
+    n /= 10;
   }
+
+  printf("Digit:      ");
+  for (digit = 0; digit <= 9; digit++)
+    printf("%3d", digit);
+  printf("\nOccurrences:");
+  for (digit = 0; digit <= 9; digit++)
+    printf("%3d", digit_count[digit]);
+  printf("\n");
+
+  return 0;
+}
 
