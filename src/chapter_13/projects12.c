5716bf55dd6a3dea87f3e9d666a4853a0e8d4fc1
root <cedarbirdcn@hotmail.com>
2013-03-29 13:20:45 +0900

    add chapter 13

---
+++ src/chapter_13/projects12.c
@@ -0,0 +1,51 @@
+/*********************************************************
+ * From C PROGRAMMING: A MODERN APPROACH, Second Edition *
+ * By K. N. King                                         *
+ * Copyright (c) 2008, 1996 W. W. Norton & Company, Inc. *
+ * All rights reserved.                                  *
+ * This program may be freely distributed for class use, *
+ * provided that this copyright notice is retained.      *
+ *********************************************************/
+
+/* projects12.c (Chapter 13, page 313) */
+/* Reverses a sentence */
+/* Multiple continous space is also considered. */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#define LENGTH 80
+
+int main(void)
+{
+  char ch, terminator = '\0', sentence[LENGTH] = {'\0'};
+  int i = 0, j, word_ending = 0;
+
+  /* read sentence - end with period, question mark or exlamation point. */
+  printf("Enter a sentence: ");
+  while ((ch = getchar()) != '\n') {
+    if (ch == '.' || ch == '?' || ch == '!')
+      terminator = ch;
+    else if (ch != ' ')
+      word_ending = i;
+    sentence[i++] = ch;
+  }
+
+  /* reverse sentence */
+  printf("Reversal of sentence: ");
+  for (i = word_ending; i >= 0; i--) {
+    ch = sentence[i];
+    if (ch == ' ') {
+      for (j = i + 1; j <= word_ending; j++)
+        putchar(sentence[j]);
+      putchar(' ');
+      word_ending = i - 1;
+    } else if (i == 0)
+      for (j = 0; j <= word_ending; j++)
+        putchar(sentence[j]);
+  }
+  printf("%c\n", terminator);
+
+  return 0;
+}
+
