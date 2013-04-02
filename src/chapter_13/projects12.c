aa1f5790a6305c6aef7a606ba00511a08f247bda
root <cedarbirdcn@hotmail.com>
2013-04-02 10:17:14 +0900

    add chapter 13

--- src/chapter_13/projects12.c
+++ src/chapter_13/projects12.c
@@ -14,38 +14,54 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define LENGTH 80
+#define MAX_WORDS 30
+#define MAX_CHARS 20
+
+char read_word(char *word);
 
 int main(void)
 {
-  char ch, terminator = '\0', sentence[LENGTH] = {'\0'};
-  int i = 0, j, word_ending = 0;
+  char terminator, words[MAX_WORDS][MAX_CHARS + 1];
+  int num_words;
 
   /* read sentence - end with period, question mark or exlamation point. */
   printf("Enter a sentence: ");
-  while ((ch = getchar()) != '\n') {
-    if (ch == '.' || ch == '?' || ch == '!')
-      terminator = ch;
-    else if (ch != ' ')
-      word_ending = i;
-    sentence[i++] = ch;
-  }
+  for (num_words = 0; num_words < MAX_WORDS; num_words++)
+    if (terminator = read_word(words[num_words]))
+      break;
 
   /* reverse sentence */
   printf("Reversal of sentence: ");
-  for (i = word_ending; i >= 0; i--) {
-    ch = sentence[i];
-    if (ch == ' ') {
-      for (j = i + 1; j <= word_ending; j++)
-        putchar(sentence[j]);
-      putchar(' ');
-      word_ending = i - 1;
-    } else if (i == 0)
-      for (j = 0; j <= word_ending; j++)
-        putchar(sentence[j]);
-  }
+  for (; num_words >= 0; num_words--)
+    printf("%s ", words[num_words]);
   printf("%c\n", terminator);
 
   return 0;
+}
+
+char read_word(char *word)
+{
+  char ch;
+
+  /* Skips precede space */
+  while ((ch = getchar()) == ' ')
+    ;
+
+  *word++ = ch;
+
+  for (;;) {
+    ch = getchar();
+    if (ch == '.' || ch == '?' || ch == '!' || ch == '\n') {
+      *word = '\0';
+      break;
+    } else if (ch == ' ') {
+      *word = ch = '\0';
+      break;
+    } else {
+      *word++ = ch;
+    }
+  }
+
+  return ch;
 }
 
