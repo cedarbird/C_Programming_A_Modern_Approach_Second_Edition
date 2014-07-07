e4fa29c0d60500ed0a1d8f410271c6b0efa560f8
cedarbird <cedarbirdcn@hotmail.com>
2014-07-07 11:05:54 +0900

    make the program more concise

--- src/chapter_06/projects01.c
+++ src/chapter_06/projects01.c
@@ -9,31 +9,26 @@
 
 /* projects01.c (Chapter 06, page 122) */
 /* Finds the largest in a serries of numbers entered by user
-   When the user enters 0 or a negative number, The program exit
-   and print the largest nonnegative number. */
+   When the user enters 0 or a negative number, The program
+   print the largest nonnegative number and exit. */
 
 #include <stdio.h>
 
 int main(void)
 {
-  float input, max;
+  float input, max = 0.0f;
 
-  /* initialize */
-  printf("Enter a number: ");
-  scanf("%f", &input);
-  max = input;
-
-  while(input > 0.0f) {
+  do {
     printf("Enter a number: ");
     scanf("%f", &input);
     if (input > max)
       max = input;
-  }
+  } while (input > 0.0f);
 
-  if (max >= 0.0f)
-    printf("The largest number entered was %.2f.\n", max);
+  if (max > 0.0f)
+    printf("The largest number entered was %f.\n", max);
   else
-    printf("The nonnegative number was not entered.\n");
+    printf("The positive number was not entered.\n");
 
   return 0;
 }
