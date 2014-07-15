d81ee0cb4b335b9345d55297cf657e5411cc3659
cedarbird <cedarbirdcn@hotmail.com>
2014-07-15 11:03:27 +0900

    add long long type

--- src/chapter_07/projects06.c
+++ src/chapter_07/projects06.c
@@ -23,6 +23,7 @@ int main(void)
   printf("Size of int: %zu\n",         sizeof(int));
   printf("Size of short: %zu\n",       sizeof(short));
   printf("Size of long: %zu\n",        sizeof(long));
+//printf("Size of long long: %zu\n",   sizeof(long long));
   printf("Size of float: %zu\n",       sizeof(float));
   printf("Size of double: %zu\n",      sizeof(double));
   printf("Size of long double: %zu\n", sizeof(long double));
