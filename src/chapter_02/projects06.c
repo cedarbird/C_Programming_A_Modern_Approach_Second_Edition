5cdc743014603aeabfd03799c07ed60f1258d33c
cedarbird <cedarbirdcn@hotmail.com>
2015-09-15 14:30:06 +0900

    remove unnecessary brackets

--- src/chapter_02/projects06.c
+++ src/chapter_02/projects06.c
@@ -21,7 +21,7 @@ int main(void)
   scanf("%d", &x);
 
   printf("The value of 3x^5 + 2x^4 - 5x^3 - x^2 + 7x - 6: %d\n",
-    (((((3 * x + 2) * x - 5) * x  - 1)  * x + 7) * x - 6));
+    ((((3 * x + 2) * x - 5) * x  - 1)  * x + 7) * x - 6);
 
   return 0;
 }
