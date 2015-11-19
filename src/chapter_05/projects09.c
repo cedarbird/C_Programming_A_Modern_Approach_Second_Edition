5f78ef75c205b481208795f43727d46ceddd4e6d
cedarbird <cedarbirdcn@hotmail.com>
2015-11-19 13:58:16 +0900

    refactoring algorithm

--- src/chapter_05/projects09.c
+++ src/chapter_05/projects09.c
@@ -12,29 +12,27 @@
 
 #include <stdio.h>
 
-#define MAXDAYS_OF_YEAR 366
-#define MAXDAYS_OF_MONTH 31
+#define YEAR_FACTOR 10000
+#define MONTH_FACTOR 100
 
 int main(void)
 {
-  int d1, m1, y1, d2, m2, y2,
-      weighing1, weighing2;
+  int d1, m1, y1, d2, m2, y2;
+  int result;
 
   printf("Enter first date (mm/dd/yy): ");
   scanf("%d /%d /%d", &m1, &d1, &y1);
   printf("Enter second date (mm/dd/yy): ");
   scanf("%d /%d /%d", &m2, &d2, &y2);
 
-  /* Computes weighing of date */
-  weighing1 = y1 * (MAXDAYS_OF_YEAR + 1) + m1 * (MAXDAYS_OF_MONTH + 1) + d1;
-  weighing2 = y2 * (MAXDAYS_OF_YEAR + 1) + m2 * (MAXDAYS_OF_MONTH + 1) + d2;
+  result = (y1 - y2) * YEAR_FACTOR + (m1 - m2) * MONTH_FACTOR + (d1 - d2);
 
-  if (weighing1 < weighing2)
-    printf("%.2d/%.2d/%.2d is earlier than %.2d/%.2d/%.2d.\n",
-            m1, d1, y1, m2, d2, y2);
-  else if (weighing1 > weighing2)
+  if (result > 0)
     printf("%.2d/%.2d/%.2d is earlier than %.2d/%.2d/%.2d.\n",
             m2, d2, y2, m1, d1, y1);
+  else if (result < 0)
+    printf("%.2d/%.2d/%.2d is earlier than %.2d/%.2d/%.2d.\n",
+            m1, d1, y1, m2, d2, y2);
   else
     printf("%.2d/%.2d/%.2d is same as %.2d/%.2d/%.2d.\n",
             m1, d1, y1, m2, d2, y2);
