f015c65bd175452637d0bfa65781e1d909918a3f
cedarbird <cedarbirdcn@hotmail.com>
2015-12-02 16:13:36 +0900

    refactoring algorithm

--- src/chapter_06/projects01.c
+++ src/chapter_06/projects01.c
@@ -16,14 +16,18 @@
 
 int main(void)
 {
-  float input, max = 0.0f;
+  float input, max;
 
-  do {
+  printf("Enter a number: ");
+  scanf("%f", &input);
+  max = input;
+
+  while (input > 0.0f) {
     printf("Enter a number: ");
     scanf("%f", &input);
     if (input > max)
       max = input;
-  } while (input > 0.0f);
+  }
 
   if (max > 0.0f)
     printf("The largest number entered was %f.\n", max);
